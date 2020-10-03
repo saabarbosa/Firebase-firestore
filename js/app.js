fd073a040542c6964a25ec649d41cd68dac0ffd0
SERGIO ANTONIO A BARBOSA <saabarbosa@gmail.com>
2020-10-03 12:52:21 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
   var firebaseConfig = {
-    apiKey: "AIzaSyDUgnMoiso9aRjz1KQpIJjpob8z2hGb9vo",
-    authDomain: "bikeshopsample-c8a1d.firebaseapp.com",
-    databaseURL: "https://bikeshopsample-c8a1d.firebaseio.com",
-    projectId: "bikeshopsample-c8a1d",
-    storageBucket: "bikeshopsample-c8a1d.appspot.com",
-    messagingSenderId: "325785968827",
-    appId: "1:325785968827:web:ad24adda69b9a66d94899e"	
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1"	
   };
 
   firebase.initializeApp(firebaseConfig);
